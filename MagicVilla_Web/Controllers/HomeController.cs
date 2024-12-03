5368f236071ff9e833075bc5fc12b603b58ffc7f
Anhhh12333333333333333333333333 <hodangquocanh2003@gmail.com>
2024-12-03 22:55:57 +0700

    Home Index

--- MagicVilla_Web/Controllers/HomeController.cs
+++ MagicVilla_Web/Controllers/HomeController.cs
@@ -1,32 +1,40 @@
+
+using AutoMapper;
 using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.dto;
+using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace MagicVilla_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+        private readonly APIResponse _response;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IVillaService villaService, IMapper mapper, APIResponse response)
         {
-            _logger = logger;
+            _villaService = villaService;
+            _mapper = mapper;
+            _response = response;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<VillaDTO> list = new();
+            var response = await _villaService.GetAllAsync<APIResponse>();
+
+
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- MagicVilla_Web/obj/Debug/net8.0/MagicVilla_Web.AssemblyInfo.cs
+++ MagicVilla_Web/obj/Debug/net8.0/MagicVilla_Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1af187bec812ee73ea316aca78195b2a10a48487")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+407a8c6693f9001f533c24521e60e5115a206cbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
